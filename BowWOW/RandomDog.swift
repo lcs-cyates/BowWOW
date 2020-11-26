36b10a89b691947dc6e7c50d6185ae497637d489
Clea Yates <cyates@lcs.on.ca>
2020-11-26 09:59:18 -0500

    done

---
+++ BowWOW/RandomDog.swift
@@ -0,0 +1,13 @@
+//
+//  RandomDog.swift
+//  BowWOW
+//
+//  Created by Yates, Clea on 2020-11-26.
+//
+
+import Foundation
+
+struct RandomDog: Codable {
+    var message: String
+    var status: String
+}
